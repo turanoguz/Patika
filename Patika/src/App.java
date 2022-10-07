c7ee9c5efeb3fdb1761d48a78528da5071e0432d
Oguzhan <turanoguzhan58@gmail.com>
2022-10-07 16:15:33 +0300

    Last Commit

--- Patika/src/App.java
+++ Patika/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        IEmployee employee = new Employee("Oğuzhan", 4000, 39, 2015);
+        IEmployee employee = new Employee("Oğuzhan", 4000, 41, 2015);
         System.out.println(employee.toString());
     }
 }
